811e7a4edfef6cf03b936c3868b8b926fa6cba71
kiruthikacharu <kiruthikag12@gmail.com>
2016-08-07 12:37:12 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,16 @@
+public class mul {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The multiplication is");
+		c=a*b;
+		System.out.println(c);
+
+	}
+
+}
